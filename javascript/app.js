7e4a2caa7eee55e9e5cf058c53540d54500bdfd0
hector guevara <hectorguevara@hectors-MacBook-Pro.local>
2017-11-10 18:29:18 -0600

    player one can submit name

--- javascript/app.js
+++ javascript/app.js
@@ -1,1 +1,19 @@
 console.log($);
+$(() => {
+let player1sub = $('<h3>');
+
+console.log(player1sub);
+
+const player1name= () => {
+  $('form').on('submit', (event) => {
+     console.log($('#player1sub').val());
+     player1sub = $("#player1sub").val();
+     event.preventDefault();
+    $(".player1").append(player1sub);
+    console.log(player1sub);
+   });
+ };
+
+player1name();
+
+})
